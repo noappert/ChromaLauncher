6e8785a2e908b18a0bf380c392c5eb44a31bd6c1
noappertBD <no.appert@gmail.com>
2024-02-21 14:06:28 +0100

    maj

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -4,7 +4,7 @@ const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = './distribution.json'
+exports.REMOTE_DISTRO_URL = 'https://raw.githubusercontent.com/noappertBD/ChromaLauncher/master/app/assets/js/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
